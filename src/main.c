6fdd77407c4b94a1ceed6dd3eadd8d2594d58659
Cameron Ewing <cameronramsayewing@gmail.com>
2025-04-01 14:28:38 -0700

    Scroller: Move sensor reading to thread
    Refactor the sensor reading and step calculation code to a thread, decoupling it
    from the communication method. This will allow for dynamic switching between
    USB and BT depending on USB connection status and type in the future.
    
    Signed-off-by: Cameron Ewing <cameronramsayewing@gmail.com>

--- src/main.c
+++ src/main.c
@@ -16,6 +16,8 @@
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
+#define SENSOR_THREAD_PRIORITY 0x02
+
 /* USB */
 static const uint8_t hid_report_desc[] = HID_WHEEL_REPORT_DESC();
 /* USB Device Status*/
@@ -29,9 +31,6 @@ struct __packed wheel_report_t
         uint8_t report_id;
         int16_t wheel;
 } wheel_report;
-
-/* Create a message queue for mouse reports to be sent over USB */
-K_MSGQ_DEFINE(mouse_msgq, sizeof(wheel_report), 2, 1);
 
 /* Semaphore for writing reports over USB. When a write is started, take the semaphore,
  * when the in endpoint is ready again release the semaphore.
@@ -76,7 +75,8 @@ static int get_report_cb(const struct device *dev, struct usb_setup_packet *setu
         ARG_UNUSED(setup);
 
         /* Get the feature report (0x0300) with report ID 2 (0x0002)*/
-        if (setup->wValue == 0x0302) {
+        if (setup->wValue == 0x0302)
+        {
                 // FIXME: Respond with the resolution multiplier
                 LOG_INF("GET_REPORT: Resolution Multiplier");
         }
@@ -151,20 +151,102 @@ int send_hid_report(const struct device *hid_dev, uint8_t *report, size_t report
         return 0;
 }
 
-int main(void)
+/* Sensor value message queue */
+K_MSGQ_DEFINE(sensor_msgq, sizeof(int16_t), 2, 1);
+
+/* Sensor read thread
+ *
+ * Responsible for reading the AS5600 sensor and enqueuing the data to be
+ * consumed by either a USB or BT sender.
+ *
+ * Thread priority is 0x02 to allow for USB/BT to run at 0x01.
+ */
+void sensor_thread_handler(void)
 {
         const struct device *sensor;
-        const struct device *hid_dev;
         int ret;
-
-        printk("Scroller v0.1 Test Application\n");
 
         sensor = DEVICE_DT_GET(DT_NODELABEL(as5600));
         if (!device_is_ready(sensor))
         {
                 LOG_ERR("Sensor not ready");
-                return 0;
+                return;
         }
+
+        // Read off the AS5600
+        ret = sensor_sample_fetch_chan(sensor, AS5600_SENSOR_CHAN_SCALED_ANGLE);
+        if (ret < 0)
+        {
+                LOG_ERR("Could not fetch samples (%d)", ret);
+                return;
+        }
+
+        struct sensor_value prev_angle;
+        struct sensor_value curr_angle;
+        /*
+         * FIXME: Apply an internal scroll accumulator. The linux kernel only supports down to
+         * (int)(steps * 120 / RES MULT) resulting a maximum of 120 steps per detent. Fractional
+         * scrolling is not supported. The sensor emits 4096/120 ~34 detents per revolution
+         * which is high.
+         *
+         * This may also be possible to solve using a custom Mouse definition in the kernel or
+         * libinput allowing for more steps per detent.
+         */
+        while (1)
+        {
+                ret = sensor_channel_get(sensor, AS5600_SENSOR_CHAN_SCALED_ANGLE, &prev_angle);
+                if (ret < 0)
+                {
+                        LOG_ERR("Could not get samples (%d)", ret);
+                        return;
+                }
+
+                k_msleep(REPORT_FREQUENCY);
+
+                ret = sensor_sample_fetch_chan(sensor, AS5600_SENSOR_CHAN_SCALED_ANGLE);
+                if (ret < 0)
+                {
+                        LOG_ERR("Could not fetch samples (%d)", ret);
+                        return;
+                }
+
+                ret = sensor_channel_get(sensor, AS5600_SENSOR_CHAN_SCALED_ANGLE, &curr_angle);
+                if (ret < 0)
+                {
+                        LOG_ERR("Could not get samples (%d)", ret);
+                        return;
+                }
+
+                /* Process the step delta accounting for roll over at 0/4095 */
+                int16_t delta = curr_angle.val1 - prev_angle.val1;
+                if (delta > 2048)
+                {
+                        delta -= 4096; // Negative direction wrap
+                }
+                else if (delta < -2048)
+                {
+                        delta += 4096; // Positive direction wrap
+                }
+
+                if (delta)
+                {
+                        // LOG_INF("Angle delta: %d", delta);
+                        delta *= -1; /* Invert delta to change scroll direction */
+                        k_msgq_put(&sensor_msgq, &delta, K_NO_WAIT);
+                }
+
+                /* Move cur to prev*/
+                prev_angle.val1 = curr_angle.val1;
+        }
+}
+K_THREAD_DEFINE(sensor_thread, 1024, sensor_thread_handler, NULL, NULL, NULL, SENSOR_THREAD_PRIORITY, 0, 0);
+
+int main(void)
+{
+        const struct device *hid_dev;
+        int ret;
+
+        printk("Scroller v0.1 Test Application\n");
 
         // Log the HID report descriptor
         LOG_HEXDUMP_INF(hid_report_desc, sizeof(hid_report_desc), "HID Report Descriptor");
@@ -186,7 +268,7 @@ int main(void)
         // Initialize the HID device
         usb_hid_init(hid_dev);
 
-        // Enable USB and binds the status callback
+        // Enable USB and binds the status callbackry april fools I feel like anton is slowly turning more and more insane.
         ret = usb_enable(status_cb);
 
         if (ret != 0)
@@ -195,79 +277,21 @@ int main(void)
                 return 0;
         }
 
-        // Read off the AS5600
-        ret = sensor_sample_fetch_chan(sensor, AS5600_SENSOR_CHAN_SCALED_ANGLE);
-        if (ret < 0)
-        {
-                LOG_ERR("Could not fetch samples (%d)", ret);
-                return 0;
-        }
-
-        struct sensor_value prev_angle;
-        struct sensor_value curr_angle;
+        struct wheel_report_t wheel_report = {
+            .report_id = 0x01,
+            .wheel = 0,
+        };
         UDC_STATIC_BUF_DEFINE(report, sizeof(wheel_report));
-        /*
-         * FIXME: Apply an internal scroll accumulator. The linux kernel only supports down to
-         * (int)(steps * 120 / RES MULT) resulting a maximum of 120 steps per detent. Fractional
-         * scrolling is not supported. The sensor emits 4096/120 ~34 detents per revolution
-         * which is high.
-         *
-         * This may also be possible to solve using a custom Mouse definition in the kernel or
-         * libinput allowing for more steps per detent.
-         */
+
         while (1)
         {
-                ret = sensor_channel_get(sensor, AS5600_SENSOR_CHAN_SCALED_ANGLE, &prev_angle);
-                if (ret < 0)
-                {
-                        LOG_ERR("Could not get samples (%d)", ret);
-                        return 0;
-                }
+                /* Wait for a message to be available */
+                k_msgq_get(&sensor_msgq, &wheel_report.wheel, K_FOREVER);
 
-                k_msleep(REPORT_FREQUENCY);
+                /* Copy the report to the static buffer */
+                memcpy(report, &wheel_report, sizeof(wheel_report));
 
-                // Log the Delta:
-                ret = sensor_sample_fetch_chan(sensor, AS5600_SENSOR_CHAN_SCALED_ANGLE);
-                if (ret < 0)
-                {
-                        LOG_ERR("Could not fetch samples (%d)", ret);
-                        return 0;
-                }
-
-                ret = sensor_channel_get(sensor, AS5600_SENSOR_CHAN_SCALED_ANGLE, &curr_angle);
-                if (ret < 0)
-                {
-                        LOG_ERR("Could not get samples (%d)", ret);
-                        return 0;
-                }
-
-                /* Process the step delta accounting for roll over at 0/4095 */
-                int16_t delta = curr_angle.val1 - prev_angle.val1;
-                if (delta > 2048)
-                {
-                        delta -= 4096; // Negative direction wrap
-                }
-                else if (delta < -2048)
-                {
-                        delta += 4096; // Positive direction wrap
-                }
-
-                /* USB */
-                if (delta)
-                {
-                        // LOG_INF("Angle delta: %d", delta);
-                        /* Prepare the report */
-                        wheel_report.report_id = 0x01;
-                        wheel_report.wheel = -1 * delta; /* Invert Scroller Direction */
-
-                        /* Copy the report to the static buffer */
-                        memcpy(report, &wheel_report, sizeof(wheel_report));
-
-                        send_hid_report(hid_dev, report, sizeof(wheel_report));
-                }
-
-                /* Move cur to prev*/
-                prev_angle.val1 = curr_angle.val1;
+                send_hid_report(hid_dev, report, sizeof(wheel_report));
         }
 
         return 0;
