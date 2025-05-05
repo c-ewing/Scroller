812f49706c1c4760e9b8725a37175d70c10ff6e7
Cameron Ewing <cameronramsayewing@gmail.com>
2025-05-04 18:42:47 -0700

    Scroller: Enable CAF settings loader
    prj.conf: Enable loading of module settings from NVS storage. Enable BLE
    settings. Enable MCUBoot bootloader to handle partions.
    
    settings_loader_def.h: Definbe which modules need settings loaded from
    flash
    
    Signed-off-by: Cameron Ewing <cameronramsayewing@gmail.com>

---
+++ configuration/common/settings_loader_def.h
@@ -0,0 +1,10 @@
+#include <caf/events/module_state_event.h>
+
+static inline void get_req_modules(struct module_flags *mf)
+{
+    module_flags_set_bit(mf, MODULE_IDX(main));
+
+#if CONFIG_CAF_BLE_ADV
+    module_flags_set_bit(mf, MODULE_IDX(ble_adv));
+#endif
+};
